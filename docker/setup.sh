9c710879ffdf501f416626cf2c8198f43ac8eee7
OsawaKousei <jwithelis@gmail.com>
2024-04-22 17:41:04 +0900

    update

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,4 +4,4 @@ sudo apt upgrade -y
 
 # make workspace and clone repository
 cd ~
-git clone https://github.com/OsawaKousei/3dpcp_ws.git
+git clone https://github.com/oreilly-japan/deep-learning-from-scratch.git
