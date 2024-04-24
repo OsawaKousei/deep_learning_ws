10abd0fa12cd7cbc7828f7d0ed6691e3e9348c03
OsawaKousei <jwithelis@gmail.com>
2024-04-24 10:06:33 +0900

    avtivation func

--- deep_learing/sec3_neural_net/activation_func.py
+++ deep_learing/sec3_neural_net/activation_func.py
@@ -1,13 +1,23 @@
+from typing import Any
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def step_function(x: np.ndarray) -> np.ndarray:
+def step_function(x: np.ndarray) -> Any:
     return np.array(x > 0, dtype=np.int32)
 
 
+def sigmoid(x: np.ndarray) -> Any:
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x: np.ndarray) -> Any:
+    return np.maximum(0, x)
+
+
 x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
+y = relu(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
