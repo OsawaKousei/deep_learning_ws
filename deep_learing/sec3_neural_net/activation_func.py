2e7850b0351d46b69009f0663984a82c02c28f8e
OsawaKousei <jwithelis@gmail.com>
2024-04-24 09:49:25 +0900

    update

---
+++ deep_learing/sec3_neural_net/activation_func.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def step_function(x: np.ndarray) -> np.ndarray:
+    return np.array(x > 0, dtype=np.int32)
+
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
